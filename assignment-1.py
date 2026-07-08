4597aee603e35b76498e93e089b6c3c4475db2a1
genesisjzm-rgb <genesisjzm@gmail.com>
2026-07-07 22:48:10 -0400

    Update assignment-1.py

--- assignment-1.py
+++ assignment-1.py
@@ -1,3 +1,55 @@
-name = 'Genesis'
+(toname = 'Genesis'
 
 print (name, type(name))
+
+name = 'Genesis'
+age = 23
+height = 5.2
+is_student = True
+
+print (name, type(name))
+print (age, type(age))
+print(height, type(height))
+print(is_student, type(is_student))
+
+name = input('what is your name? ')
+age = input('how old are you? ')
+
+
+print ("Hi!", name + "you are approximatly", age, "years old")
+num1 = input("Enter a number (decimal)")
+num2 = input("Enter another number (decimal)")
+
+num1 = input("Enter a number (decimal)")
+num2 = input("Enter another number (decimal)")
+
+
+num1 = float(num1)
+num2 = float(num2)
+
+total = num1 * num2 
+print (f"Total: {total:.2f}")
+
+Item = "Python textbook"
+Price = 59.99
+Quantity = 5
+
+total = Price * Quantity
+
+print(f"Item: {Item}")
+print(f"Price: ${Price:.2f}")
+print(f"Quantity: {Quantity}")
+print(f"Total: ${total:.2f}")
+name = input("What is your name? ")
+hometown = input("What is your hometown? ")
+hobby = input("what is your favorite hobby? ")
+funFact = input("What is one fun fact about yourself?")
+Year = int(input("What is the year you were born?"))
+
+Age = 2026 - Year
+
+print(f"PROFILE: {name}")
+print(f"Hometown: {hometown}")
+print(f"Fun Fact: {funFact}")
+print(f"Age: {Age}")
+
